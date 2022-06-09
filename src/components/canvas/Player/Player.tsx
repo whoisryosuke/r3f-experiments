3583f13231f7ffe453d8de9ae91042e304aa9eac
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-06-09 16:12:47 -0700

    :zap: better movement handling

--- src/components/canvas/Player/Player.tsx
+++ src/components/canvas/Player/Player.tsx
@@ -9,6 +9,8 @@ type Props = Partial<GroupProps> & {
   disabled?: boolean;
 };
 
+const MOVE_MULTIPLIER = 0.1;
+
 const Player = ({ disabled = false, ...props }: Props) => {
   // const position = useRef<Vector3>([0, 0, 0]);
   const playerRef = useRef<Mesh>();
@@ -17,7 +19,29 @@ const Player = ({ disabled = false, ...props }: Props) => {
     // console.log("input", input.controls.move.value);
     // Jump command
     if (input.controls.fire.value) {
-      playerRef.current.position.set(0, 1, 0);
+      let newY = playerRef.current.position.y + 1;
+
+      // Limit jump height
+      if (newY > 1) {
+        newY = 1;
+      }
+      playerRef.current.position.set(0, newY, 0);
+    }
+    // Movement
+    if (input.controls.move.value) {
+      // How much did we move in each direction?
+      // We use the input (which is -1 to 1)
+      // We invert the Z to accomodate how it increments (e.g. deeper = neg, but forward = positive on input)
+      const inputX = input.controls.move.value.x * MOVE_MULTIPLIER;
+      const inputZ = input.controls.move.value.y * -1 * MOVE_MULTIPLIER;
+
+      // Add the current position with the movement amount
+      const newX = playerRef.current.position.x + inputX;
+      const newZ = playerRef.current.position.z + inputZ;
+
+      // We save Y just in case it changes? Also just shorter
+      const oldY = playerRef.current.position.y;
+      playerRef.current.position.set(newX, oldY, newZ);
     }
   });
 
